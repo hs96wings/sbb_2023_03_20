e4de8d2a675396e22eb4a63ded4b2221737e3685
hs96wings <hs96wings@gmail.com>
2023-03-20 14:31:41 +0900

    MainController

---
+++ src/main/java/com/mysite/sbb/Entity/Question.java
@@ -0,0 +1,11 @@
+package com.mysite.sbb.Entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+@Getter
+@Setter
+public class Question {
+}
