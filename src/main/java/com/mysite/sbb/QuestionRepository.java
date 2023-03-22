cd6f73fcf8610569522fcfec4a8ad7cc6d2bb9a6
hs96wings <hs96wings@gmail.com>
2023-03-22 09:27:53 +0900

    각 테스트케이스의 독립성 확보

--- src/main/java/com/mysite/sbb/QuestionRepository.java
+++ src/main/java/com/mysite/sbb/QuestionRepository.java
@@ -1,5 +1,6 @@
 package com.mysite.sbb;
 
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -13,6 +14,8 @@ public interface QuestionRepository extends JpaRepository<Question, Integer> {
 
     List<Question> findBySubjectLike(String subject);
 
+    @Transactional
+    @Modifying // 아래 Query가 SELECT가 아니라면 붙여야 한다
     // nativeQuery = true여야 MySQL Query 문법 사용가능
     @Query(value = "ALTER TABLE question AUTO_INCREMENT = 1", nativeQuery = true)
     void clearAutoIncrement();
