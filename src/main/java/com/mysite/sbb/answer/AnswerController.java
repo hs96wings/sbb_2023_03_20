a767c6d008a82b9d345f85d5efbb018594671f4c
hs96wings <hs96wings@gmail.com>
2023-03-22 14:27:45 +0900

    답변 컨트롤러 만들기

---
+++ src/main/java/com/mysite/sbb/answer/AnswerController.java
@@ -0,0 +1,26 @@
+package com.mysite.sbb.answer;
+
+import com.mysite.sbb.question.Question;
+import com.mysite.sbb.question.QuestionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/answer")
+@RequiredArgsConstructor
+public class AnswerController {
+    private final QuestionService questionService;
+    @GetMapping("/create/{id}")
+    public String createAnswer(Model model, @PathVariable Integer id, @RequestParam String content) {
+        Question question = questionService.getQuestion(id);
+
+        // TODO: 답변 객체를 만든다
+        return "redirect:/question/detail/%d".formatted(id);
+    }
+
+}
