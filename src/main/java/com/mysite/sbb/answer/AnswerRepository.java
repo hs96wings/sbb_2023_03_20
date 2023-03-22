154996ec9f4bd9233f1e03bcb9fee23c811dc2a7
hs96wings <hs96wings@gmail.com>
2023-03-22 15:45:17 +0900

    관련 답변 표시v1

--- src/main/java/com/mysite/sbb/answer/AnswerRepository.java
+++ src/main/java/com/mysite/sbb/answer/AnswerRepository.java
@@ -5,10 +5,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
+
 public interface AnswerRepository extends JpaRepository<Answer, Integer> {
     @Transactional
     @Modifying // 아래 Query가 SELECT가 아니라면 붙여야 한다
     // nativeQuery = true여야 MySQL Query 문법 사용가능
     @Query(value = "ALTER TABLE answer AUTO_INCREMENT = 1", nativeQuery = true)
     void clearAutoIncrement();
+
+    List<Answer> findByQuestionId(Integer id);
 }
